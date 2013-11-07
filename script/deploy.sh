a60023fe025ee3169c25b089b5fd967e9090af87
Samuli Thomasson <simsaladin@paivola.fi>
2013-11-08 00:27:59 +0200

    deploy combinet

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,5 +3,6 @@
 cabal clean \
    && cabal configure -fproduction \
    && cabal build \
+   && scp -r static/combined functor:/srv/sites/mustached-octo-happiness/static \
    && scp dist/build/mustached-octo-happiness/mustached-octo-happiness \
           functor:/srv/sites/mustached-octo-happiness/binary
