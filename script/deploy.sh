50d79087443e086213ba0e11f613327051bfe4b8
Samuli Thomasson <simsaladin@paivola.fi>
2013-12-04 14:35:28 +0200

    complicate deploy script a bit

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+HOST=functor
+MOH_PATH=/srv/sites/mustached-octo-happiness
+BIN=mustached-octo-happiness
+
 cabal clean \
    && cabal configure -fproduction \
    && cabal build \
-   && rsync -r static/ \
-             functor:/srv/sites/mustached-octo-happiness/static \
-   && scp -C dist/build/mustached-octo-happiness/mustached-octo-happiness \
-          functor:/srv/sites/mustached-octo-happiness/binary.new \
-   && ssh -t functor 'bash -c "sudo systemctl stop mustached-octo-happiness \
-   && mv /srv/sites/mustached-octo-happiness/{binary,binary.old} \
-   && mv /srv/sites/mustached-octo-happiness/{binary.new,binary} \
-   && sudo systemctl start mustached-octo-happiness"'
+   && rsync -r static/ $HOST:$MOH_PATH/static \
+   && scp -C dist/build/mustached-octo-happiness/$BIN-octo-happiness \
+          $HOST:$MOH_PATH/binary.new \
+   # attract the user, as following sudo's likely require interaction.
+   && echo -en '' \
+   && ssh -t functor "bash \
+   -c 'sudo systemctl stop mustached-octo-happiness \
+   && mv $MOH_PATH/{binary,binary.old} \
+   && mv $MOH_PATH/{binary.new,binary} \
+   && sudo systemctl start mustached-octo-happiness'"
           # xxx: trap for unsuccesful mv's?
+          # xxx: clean up &&'s with that bash's exit-on-failed-command hook?
