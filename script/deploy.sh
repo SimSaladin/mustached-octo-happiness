f0eedbf3adefeecb97d006b2ea5cb7eb98d016ba
Samuli Thomasson <simsaladin@paivola.fi>
2013-12-08 00:51:20 +0200

    Fix deploy script, frontpage image width

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 HOST=functor
+NAME=mustached-octo-happiness
 MOH_PATH=/srv/sites/mustached-octo-happiness
-BIN=mustached-octo-happiness
 
 cabal clean \
    && cabal configure -fproduction \
    && cabal build \
    && rsync -r static/ $HOST:$MOH_PATH/static \
-   && scp -C dist/build/mustached-octo-happiness/$BIN-octo-happiness \
+   && scp -C dist/build/$NAME/$NAME \
           $HOST:$MOH_PATH/binary.new \
-   # attract the user, as following sudo's likely require interaction.
    && echo -en '' \
    && ssh -t functor "bash \
-   -c 'sudo systemctl stop mustached-octo-happiness \
+   -c 'sudo systemctl stop $NAME \
    && mv $MOH_PATH/{binary,binary.old} \
    && mv $MOH_PATH/{binary.new,binary} \
-   && sudo systemctl start mustached-octo-happiness'"
+   && sudo systemctl start $NAME'"
           # xxx: trap for unsuccesful mv's?
           # xxx: clean up &&'s with that bash's exit-on-failed-command hook?
